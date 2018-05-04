b608e693366bdd52ae2b10c170bf1baa4ea6203f
zhiwutian <brettalbright@outlook.com>
2018-05-04 12:37:26 -0700

    completed axios demo

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 const BASE_URL = 'http://api.reactprototypes.com';
-const API_KEY = '?key=testuser1234';
+const API_KEY = '?key=c118demouser';
 
-axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
-    const { todos } = resp.data;
-    const table = $('table tbody');
+axios.get(`${BASE_URL}/todos${API_KEY}`).then(response => {
+    console.log("Response", response);
 
-    addToDom(todos, table);
-});
+    const listElements = response.data.todos.map((item, index) => {
+        const tr = document.createElement("tr");
+        const number = document.createElement("td");
+        const title = document.createElement("td");
+        const completed = document.createElement("td");
 
-const addToDom = (list, container) => {
-    const tableRows = list.map((item, index) => {
+        number.innerText = index + 1;
+        title.innerText = item.title;
+        completed.innerText = item.complete;
 
-        const tableData = [
-            $(`<td>${index + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
+        tr.appendChild(number);
+        tr.appendChild(title);
+        tr.appendChild(completed);
 
-        return $('<tr>').append(tableData);
+        document.querySelector(".table tbody").appendChild(tr);
+
     });
 
-    container.append(tableRows);
+
+
+
+});
+
+const todoItem = {
+    tite: "My Own Todo Item",
+    details: "just trying out todos"
 };
+
+axios.post(`${BASE_URL}/todos${API_KEY}`, todoItem).then(resp => {
+    console.log("Add Item Resp:", resp);
+}).catch( err => {
+    console.log("Error", err.response.data.error);
+
+    console.log(err.message);
+
+    const newError = new Error("This is the error message");
+    console.log("New Error", newError.message);
+
+    throw(newError);
+
+    alert(err.response.data.error);
+});
+
